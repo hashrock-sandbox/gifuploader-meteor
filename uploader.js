7be5045ae8671779467a1ad163bdcf4c7b05d5b2
hashrock <hashedrock@gmail.com>
2015-06-05 13:56:30 +0900

    initial commit

---
+++ uploader.js
@@ -0,0 +1,36 @@
+var imageStore = new FS.Store.GridFS("images");
+Images = new FS.Collection("images", {
+ stores: [imageStore]
+});
+if (Meteor.isClient) {
+  Template.main.helpers({
+    counter: function () {
+      return Session.get('counter');
+    },
+    images: function () {
+      return Images.find({}, {sort: {uploadedAt: -1}});
+    }
+  });
+  Template.image.events({
+    "click .removeImage": function(){
+      Images.remove(this._id);
+    }
+  });
+  Template.main.events({
+    'change .myFileInput': function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        Images.insert(file, function (err, fileObj) {
+          if (err){
+          }
+          // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+        });
+      });
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
